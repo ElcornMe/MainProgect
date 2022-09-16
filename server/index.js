ee636441b0649c2f0fcbe8f38b8102486762676e
Denys <elcornme@gmail.com>
2022-09-16 10:54:12 +0300

    CreatedServer

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const sequelize = require('./db');
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`server started on ${PORT}`));  
+    } catch (e) {
+        console.log(e.name)
+        console.log(e.message)
+    }
+};
+
+app.get('/', function (req, res) {
+    res.sendStatus(200);
+    res.send("<h2>Online Shop<h2>");  
+});
+
+start();
