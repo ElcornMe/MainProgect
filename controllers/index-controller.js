335e1f2cbdc82d1d08f4412799f265d70caeab72
Denys <elcornme@gmail.com>
2022-09-23 10:20:56 +0300

    Models for db

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,7 +1,18 @@
+const { User } = require('../database/db-model-user');
+
 const getIndex = (req, res) => {
   res.render('index', {title: 'Online Shop'});   
-}
+};
+
+const postIndex = (req, res) => {
+  User.findAll({where:{name: "Tom"}, raw: true })
+    .then(users=>{
+      console.log(users);
+    }).catch(err=>console.log(err));
+  res.status(200).json('return response')
+};
 
 module.exports = {
-  getIndex
+  getIndex,
+  postIndex
 };
--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var { getIndex } = require('../controllers/index-controller')
+var { getIndex, postIndex } = require('../controllers/index-controller')
 
 router.get('/', getIndex);
+router.post('/', postIndex);
 
 module.exports = router;
