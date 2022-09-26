6509119bc620357a2cdc660f101f51a752e36250
Denys <elcornme@gmail.com>
2022-09-26 18:40:03 +0300

    unimportant changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
 const path = require('path');
-require('dotenv').config()
+require('dotenv').config();
 
 var indexRouter = require('./routes/index-routes');
 var catalogRouter = require('./routes/catalog-routes');
 var cartRouter = require('./routes/cart-routes');
 var goodsRouter = require('./routes/goods-routes');
 
-const app = express()
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
