39076b1544b51c89ab4097c66034d5eacec9be4b
Denys <elcornme@gmail.com>
2022-09-26 18:44:45 +0300

    connect to db

---
+++ config/dbConfig.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const dev = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: 5432,
+  dialect: 'postgres',
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  },
+  logging: (msg) => {
+    // console.log(msg);
+  },
+  define: { createdAt: false },
+};
+
+module.exports = { development: dev, test: null, production: null };
