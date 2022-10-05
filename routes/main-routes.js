d29b9dab71c6883ca70a182c22db65bbc636348b
Denys <elcornme@gmail.com>
2022-10-05 10:02:50 +0300

    delete post method

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var { getMain, postMain } = require('../controllers/main-controller')
+var { getMain } = require('../controllers/main-controller')
 
 router.get('/', getMain);
-router.post('/', postMain);
 
 module.exports = router;
