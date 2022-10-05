95463aabaa1f5f6a252cb29ec0992bc76995f268
Denys <elcornme@gmail.com>
2022-10-05 13:41:08 +0300

    Revert

--- database/models/index.js
+++ database/models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-require("dotenv").config();
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -8,27 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/dbConfig.js')[env];
 const db = {};
 
-
 let sequelize;
 if (false) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
-)
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
