1ebf0ceafd3fd666e78dcb999958bd4aed55ccf9
Denys <elcornme@gmail.com>
2022-10-16 15:36:37 +0300

    changed find method into database

--- controllers/registration-controller.js
+++ controllers/registration-controller.js
@@ -9,17 +9,17 @@ const getRegistration = (req, res) => {
 const postRegistration = async (req, res) => {
   const { email } = req.body;
   
-  const checkEmail = await model.findAll({where:{email: email}, raw: true })
-    .then(users=> {
-        if(users.length > 0) return true
-      })
-    .catch(err=> console.log(err));
+  const checkEmail = await model.findOne({where:{email: email}, raw: true })
+  .then(user=> {
+      if(user) return true
+    })
+  .catch(err=> console.log(err));
 
   if(checkEmail) {
     res.render('error', {cap: "Wrong!",userEmail:`${email}`, err: "Already registered, if it's yours please try"});
   }else {
     model.create({...req.body});
-    res.render('error', {cap: "Congratulations!",userEmail:`${email}`, err: "Have successfully registered, please proceed to"});
+    res.render('success', {cap: "Congratulations!",userEmail:`${email}`, err: "Have successfully registered, please proceed to"});
   }
 };
 
