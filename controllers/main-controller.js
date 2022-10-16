a3a0326cf82a5a42e9b40ebd1cdbcf90cd9d58aa
Denys <elcornme@gmail.com>
2022-10-16 15:37:59 +0300

    added display info about user

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,6 +1,18 @@
-const getMain = (req, res) => {
-  res.render('main', {title: 'Online Shop', name: 'User'});   
+const models = require('../database/models');
+
+const model = models.User;
+
+
+const getMain = async (req, res) => {
+    
+  const findUserById = await model.findOne({where: {id: req.cookies.userId}})
+  .then(user => {
+    let userName = user.name;
+    let userCity = user.city;
+    res.render('main', {title: 'Online Shop', name: `${userName}`, city: userCity}); 
+  })
 };
+
 
 module.exports = {
   getMain
