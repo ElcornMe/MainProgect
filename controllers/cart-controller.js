09ae41ebd7fabb10aaedcaac7a1fe1efc4b3d1dd
Denys <elcornme@gmail.com>
2022-10-16 15:50:20 +0300

    added checkup items in the cart

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,5 +1,23 @@
-const getCart = (req, res) => {
-  res.render('cart');  
+const models = require('../database/models');
+
+let model = models.Order;
+
+const getCart = async (req, res) => {
+
+let userId = req.cookies.userId;
+
+const checkOrdersInCart = await model.findAll({where: {userId: userId}}) 
+  .then((user) => {
+    if(user.length) {
+      
+    }else{
+      res.send('Cart is empty')
+    }
+  })
+
+  if(checkOrdersInCart) {
+    res.render('cart');
+  }
 }
 
 module.exports = {
