212ebed76716aa0d560a0573feb1f14f3bce363d
Denys <elcornme@gmail.com>
2022-10-16 15:45:50 +0300

    added selection catalogs from the database

--- controllers/catalog-controller.js
+++ controllers/catalog-controller.js
@@ -1,7 +1,36 @@
+const models = require('../database/models');
+
+const model = models.Catalog;
+
+
+const addCatalog = (req, res) => {
+  
+  model.create({
+      title: req.body.title
+    }).then(res=>{
+      console.log(res);
+    }).catch(err=>console.log(err));
+
+  };
+
+
 const getCatalog = (req, res) => {
-  res.render('catalog');  
-}
+
+  model.findAll({raw:true})
+  .then((catalog) => {
+    let arrOfCatalogs = [];
+
+    for(let i = 0; i < catalog.length; i++) {
+      arrOfCatalogs[i] = catalog[i].title
+    }
+
+    res.render('catalog', {raw: arrOfCatalogs}); 
+    
+  })
+};
+
 
 module.exports = {
-  getCatalog
+  getCatalog,
+  addCatalog
 };
