2c9fa529985725a5e111b78b7228206fe91efce0
Denys <elcornme@gmail.com>
2022-10-16 15:27:02 +0300

    added middleware function for authenticate

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors')
 const express = require('express');
 var cookieParser = require('cookie-parser');
 const path = require('path');
@@ -19,12 +20,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+function auth (req, res, next) {
+
+    if(!req.cookies.userId) {
+        res.render('error', {cap: "Authorize is failing , please Re-Log in:"});
+    };
+    next()
+
+ };
+
+
+app.use('/registration', registrationRouter);
 app.use('/', indexRouter);
+app.use(auth);
 app.use('/main', mainRouter);
 app.use('/catalog', catalogRouter);
 app.use('/cart', cartRouter);
 app.use('/goods', goodsRouter);
-app.use('/registration', registrationRouter);
+
 
 
 module.exports = app;
