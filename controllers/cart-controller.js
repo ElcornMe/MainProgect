48858db78bc0ccd296c36b69fbfccec58e437b14
Denys <elcornme@gmail.com>
2022-10-18 16:21:57 +0300

    added test code

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,23 +1,39 @@
 const models = require('../database/models');
 
-let model = models.Order;
+let Order = models.Order;
+let Product = models.Product;
+
+
 
 const getCart = async (req, res) => {
+  let userId = req.cookies.userId;
 
-let userId = req.cookies.userId;
+  const checkOrdersInCart = await Order.findOne({
+    where: {userId: userId}
+  }) 
+    .then((order) => {
+      // if(order.length) {
+      //   let arrOfOrders = [];
 
-const checkOrdersInCart = await model.findAll({where: {userId: userId}}) 
-  .then((user) => {
-    if(user.length) {
-      
-    }else{
-      res.send('Cart is empty')
-    }
-  })
+      //     for(let i = 0; i < order.length; i++) {
+      //       arrOfOrders[i] = order[i].name
+      //       console.log(order[i])
+      //     }
 
-  if(checkOrdersInCart) {
-    res.render('cart');
-  }
+      //   res.render('cart', { orders: arrOfOrders })
+      // }else{
+      //   res.render('cart')
+      // }
+
+      console.log(order)
+    //   order.getProducts()
+    //   .then(products=>{
+    //     for(product of products){
+    //         console.log("course:", product.id );
+    //     }
+    // })
+    res.render('cart')
+})
 }
 
 module.exports = {
