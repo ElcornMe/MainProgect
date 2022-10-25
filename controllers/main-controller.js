5a965b8f39eb0f7d602e1862b96ae5c58d8c5f45
Denys <elcornme@gmail.com>
2022-10-25 11:31:39 +0300

    deleted unnecessary code

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,20 +1,11 @@
 const models = require('../database/models');
 
-const model = models.User;
-
 
 const getMain = async (req, res) => {
 
-  const findUserById = await model.findOne({where: {id: req.cookies.userId}})
-  .then(user => {
-    let userName = user.name;
-    let userCity = user.city;
-    res.render('main', {title: 'Online Shop', name: `${userName}`, city: userCity}); 
-  })
-  .catch(err => {
-    console.log(err)
-  })
-};
+  res.render('main', {title: 'Online Shop'}); 
+
+}
 
 
 module.exports = {
